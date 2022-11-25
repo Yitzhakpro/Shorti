bf594d52bc21a5358d34160f82ef15dd98598b33
Yitzhakpro <isako14@gmail.com>
2022-11-25 18:08:03 +0200

    added react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="App"></div>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
