5e71dbac9632e922024ac2f1b48ee0994e14a8c6
Yitzhakpro <isako14@gmail.com>
2022-12-08 15:19:25 +0200

    used logout implementation on logout button

--- src/components/Layout/Navbar/LoggedInSection/LoggedInSection.tsx
+++ src/components/Layout/Navbar/LoggedInSection/LoggedInSection.tsx
@@ -4,12 +4,18 @@ import { useAuth } from '../../../../hooks';
 import './loggedInSection.css';
 
 function LoggedInSection(): JSX.Element {
-  const { username } = useAuth();
+  const { username, logout } = useAuth();
+
+  const handleLogout = async (): Promise<void> => {
+    await logout();
+  };
 
   return (
     <div className="logged-in-section">
       <Typography>Welcome, {username}</Typography>
-      <Button className="logout-btn">Logout</Button>
+      <Button className="logout-btn" onClick={handleLogout}>
+        Logout
+      </Button>
     </div>
   );
 }
