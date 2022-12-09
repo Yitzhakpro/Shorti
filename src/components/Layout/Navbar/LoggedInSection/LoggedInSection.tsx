1582c262ec74e78c15d956e8c2bbc51e9dcb2f5d
Yitzhakpro <isako14@gmail.com>
2022-12-10 00:38:29 +0200

    changed style of navbar to signed in profile pic

--- src/components/Layout/Navbar/LoggedInSection/LoggedInSection.tsx
+++ src/components/Layout/Navbar/LoggedInSection/LoggedInSection.tsx
@@ -1,22 +1,56 @@
-import React from 'react';
-import { Typography, Button } from '@mui/material';
+import React, { useState } from 'react';
+import { Box, Tooltip, IconButton, Avatar, Menu, MenuItem, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../../../hooks';
 import './loggedInSection.css';
 
 function LoggedInSection(): JSX.Element {
-  const { username, logout } = useAuth();
+  const navigate = useNavigate();
+  const { logout } = useAuth();
+
+  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
   const handleLogout = async (): Promise<void> => {
     await logout();
+    navigate('/');
   };
 
   return (
-    <div className="logged-in-section">
-      <Typography>Welcome, {username}</Typography>
-      <Button className="logout-btn" onClick={handleLogout}>
-        Logout
-      </Button>
-    </div>
+    <Box sx={{ flexGrow: 0 }}>
+      <Tooltip title="Open settings">
+        <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+          <Avatar />
+        </IconButton>
+      </Tooltip>
+      <Menu
+        sx={{ mt: '45px' }}
+        id="menu-appbar"
+        anchorEl={anchorElUser}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        keepMounted
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        open={Boolean(anchorElUser)}
+        onClose={handleCloseUserMenu}
+      >
+        <MenuItem onClick={handleLogout}>
+          <Typography textAlign="center">Logout</Typography>
+        </MenuItem>
+      </Menu>
+    </Box>
   );
 }
 
--- src/components/Layout/Navbar/Navbar.tsx
+++ src/components/Layout/Navbar/Navbar.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, Toolbar, Box, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../../../hooks';
 import LoggedInSection from './LoggedInSection';
 import LoggedOutSection from './LoggedOutSection';
 import './navbar.css';
+
+const pages = [
+  { name: 'My Links', link: '/u/allLinks' },
+  { name: 'Create Links', link: '/u/createLinks' },
+];
 
 function Navbar(): JSX.Element {
   const { isLoggedIn } = useAuth();
@@ -12,9 +17,17 @@ function Navbar(): JSX.Element {
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography className="shorti-title" variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        <Typography className="shorti-title" variant="h6" component="div">
           <Link to="/">Shorti</Link>
         </Typography>
+        <Box sx={{ flexGrow: 1, display: { md: 'flex' } }}>
+          {pages.map((pageInfo) => (
+            <Link key={pageInfo.name} to={pageInfo.link}>
+              {pageInfo.name}
+            </Link>
+          ))}
+        </Box>
+
         {isLoggedIn ? <LoggedInSection /> : <LoggedOutSection />}
       </Toolbar>
     </AppBar>
