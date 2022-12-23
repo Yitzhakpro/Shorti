f441d844665bf1e844a87729fee4598a42c3de08
Yitzhakpro <isako14@gmail.com>
2022-12-24 00:25:44 +0200

    changed the way of fetching links

--- src/components/Links/LinksList/LinksList.tsx
+++ src/components/Links/LinksList/LinksList.tsx
@@ -1,23 +1,56 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Container } from '@mui/material';
-import { useQuery } from '@tanstack/react-query';
 import { Links } from '../../../services';
 import { SkeletonList } from '../../../utilComponents';
 import LinkItem from '../LinkItem';
+import type { UrlInfo } from '../../../types';
+import './linksList.css';
 
 function LinksList(): JSX.Element {
-  const { isLoading, data, isError } = useQuery({
-    queryKey: ['getOwnShortUrls'],
-    queryFn: () => Links.getOwnShortUrls(),
-  });
+  const [isLoading, setIsLoading] = useState(true);
+  const [linksList, setLinksList] = useState<UrlInfo[]>([]);
+  const [isError, setIsError] = useState(false);
+
+  // TODO: re-think in react query
+  useEffect(() => {
+    setIsLoading(true);
+    setIsError(false);
+
+    Links.getOwnShortUrls()
+      .then((links) => {
+        setLinksList(links);
+        setIsLoading(false);
+      })
+      .catch((_error) => {
+        setIsError(true);
+        setIsLoading(false);
+      });
+  }, []);
+
+  const handleDeleteLink = useCallback(
+    async (id: string): Promise<void> => {
+      try {
+        await Links.deleteShortUrl(id);
+
+        const filteredLinks = linksList.filter((urlInfo) => {
+          return urlInfo.id !== id;
+        });
+
+        setLinksList(filteredLinks);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    [linksList]
+  );
 
   return (
-    <Container maxWidth="md">
+    <Container className="links-list" maxWidth="md">
       {isLoading && <SkeletonList variant="rectangular" height={80} rows={4} />}
       {isError && <p>error fetching</p>}
-      {data &&
-        data.map((urlInfo) => {
-          return <LinkItem key={urlInfo.id} linkInfo={urlInfo} />;
+      {linksList &&
+        linksList.map((urlInfo) => {
+          return <LinkItem key={urlInfo.id} linkInfo={urlInfo} handleDelete={handleDeleteLink} />;
         })}
     </Container>
   );
