b6697364aab5d3f2e0d75e4f73274ea9725be005
Yitzhakpro <isako14@gmail.com>
2022-12-24 00:46:30 +0200

    WIP: started working on modal of adding links (added speed dial)

--- src/components/Links/LinksList/LinksList.tsx
+++ src/components/Links/LinksList/LinksList.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { Container } from '@mui/material';
+import Add from '@mui/icons-material/Add';
+import { Container, SpeedDial, SpeedDialAction, SpeedDialIcon } from '@mui/material';
 import { Links } from '../../../services';
 import { SkeletonList } from '../../../utilComponents';
 import LinkItem from '../LinkItem';
@@ -45,14 +46,24 @@ function LinksList(): JSX.Element {
   );
 
   return (
-    <Container className="links-list" maxWidth="md">
-      {isLoading && <SkeletonList variant="rectangular" height={80} rows={4} />}
-      {isError && <p>error fetching</p>}
-      {linksList &&
-        linksList.map((urlInfo) => {
-          return <LinkItem key={urlInfo.id} linkInfo={urlInfo} handleDelete={handleDeleteLink} />;
-        })}
-    </Container>
+    <>
+      <Container className="links-list" maxWidth="md">
+        {isLoading && <SkeletonList variant="rectangular" height={80} rows={4} />}
+        {isError && <p>error fetching</p>}
+        {linksList &&
+          linksList.map((urlInfo) => {
+            return <LinkItem key={urlInfo.id} linkInfo={urlInfo} handleDelete={handleDeleteLink} />;
+          })}
+      </Container>
+
+      <SpeedDial
+        ariaLabel="SpeedDial basic example"
+        sx={{ position: 'absolute', bottom: 16, right: 16 }}
+        icon={<SpeedDialIcon />}
+      >
+        <SpeedDialAction icon={<Add />} tooltipTitle="Add Shorti" />
+      </SpeedDial>
+    </>
   );
 }
 
