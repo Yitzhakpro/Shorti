3bfd690b0a40fc49e53aa64eb5a2147326e40630
Yitzhakpro <isako14@gmail.com>
2022-12-29 17:46:02 +0200

    added username to homepage if logged in

--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Container, Typography } from '@mui/material';
+import { useAuth } from '../../hooks';
 import './homepage.css';
 
 function Homepage(): JSX.Element {
+  const { isLoggedIn, username } = useAuth();
+
   return (
     <Container className="homepage">
       <div className="homepage-intorduction">
-        <Typography variant="h4">Welcome to Shorti</Typography>
+        <Typography variant="h4">Welcome to Shorti{isLoggedIn && `, ${username}`}</Typography>
         <Typography variant="h5">You can make your links short here!</Typography>
       </div>
     </Container>
